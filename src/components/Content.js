d7d491f719d152b4d034c076e0699ae25f4ac310
behnamsaba <behnams71@gmail.com>
2023-05-30 19:29:58 -0700

    deploy bug

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,17 +1,23 @@
 const Content = ({ title, items, Component }) => {
+    if (!items || !Array.isArray(items)) {
+      // Handle the case when items is undefined or not an array
+      return null; // or return an appropriate fallback UI
+    }
+  
     return (
-        <section className='px-5 py-10'>
-            <h1 className='text-3xl font-bold underline text-center py-3 mb-8'>{title}</h1>
-            <div className='grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6'>
-                {items.map((item) => (
-                    <Component
-                        key={item.id}
-                        {...item}
-                    />
-                ))}
-            </div>
-        </section>
+      <section className='px-5 py-10'>
+        <h1 className='text-3xl font-bold underline text-center py-3 mb-8'>{title}</h1>
+        <div className='grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6'>
+          {items.map((item) => (
+            <Component
+              key={item.id}
+              {...item}
+            />
+          ))}
+        </div>
+      </section>
     );
-};
-
-export default Content;
+  };
+  
+  export default Content;
+  
